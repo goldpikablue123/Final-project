164dfeb5ebc2abf0eef566fb91175619d11f7700
Paxton Orchard <157506185+goldpikablue123@users.noreply.github.com>
2026-05-06 16:27:53 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Hello World";
+#Include<iostream>
+#include<string>
+//what to do
+
+
+Int main()
+{
+
+
+Class grade
+[
+
+Int grade;
+Int assingmentNumber;
+Cin<<’enter number of assingments’<<assingmentNumber;
+
+
+
+]
+
+
+
+
+
+Cout<>’thank you for using this program’
+
+
+
+
+}
+
+    return 0;
+}
